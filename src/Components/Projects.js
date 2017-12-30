791100e7e7c97efe9364dc585658e66d34e79073
JayLatt <jlattimor3@gmail.com>
2017-12-30 03:12:50 -0500

    used flex display on the projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,70 +4,67 @@ import '../assets/styles/Projects.css';
 class Projects extends Component {
   render() {
     return (
-      <div className="contain">
+      <div>
         <section className="project-head">
           <h3>PROJECTS</h3>
         </section>
         <section className="project-list">
-          <div className="model-1">
+          <div className="model-1 slim">
             <aside>
               <h4>Crypto Central</h4>
               <p>React.js project using AJAX to pull data from coinmarketcap.com</p>
             </aside>
             <article>
-              {/* image placeholder */}
-
+              <img alt="Crypto Central" src={require("../assets/images/crypto-central.PNG")}/>
             </article>
           </div>
-          <div className="model-2">
-            <article>
-              {/* image placeholder */}
-
-            </article>
+          <div className="model-2 slim">
             <aside>
+              <img alt="Mock Pong" src={require("../assets/images/mock-pong.PNG")}/>
+            </aside>
+            <article>
               <h4>Mock Pong</h4>
               <p>An HTML5 game using Canvas.</p>
-            </aside>
+            </article>
           </div>
-          <div className="model-1">
+          <div className="model-3 slim">
             <aside>
               <h4>Mocipedia</h4>
               <p>Ruby on Rails based Wikipedia copycat.</p>
             </aside>
             <article>
-              {/* image placeholder */}
-
+              <img alt="Mockipedia" src={require("../assets/images/mockipedia.PNG")}/>
             </article>
           </div>
-          <div className="model-2">
-            <article>
-              {/* image placeholder */}
-
-            </article>
+          <div className="model-4 slim">
             <aside>
+              <img alt="Mockit Off" src={require("../assets/images/mockit-off.PNG")}/>
+
+            </aside>
+            <article>
               <h4>Mockit Off</h4>
               <p>Angularfire powered checklist with expiration after 3 min and priority system!</p>
-            </aside>
+            </article>
           </div>
-          <div className="model-1">
+          <div className="model-1 slim">
             <aside>
               <h4>PSD Test</h4>
               <p>PSD to HTML exercise.</p>
             </aside>
             <article>
-              {/* image placeholder */}
+              <img alt="PSD Test" src={require("../assets/images/psd-practice.PNG")}/>
 
             </article>
           </div>
-          <div className="model-2">
-            <article>
-              {/* image placeholder */}
-
-            </article>
+          <div className="model-2 slim">
             <aside>
+              <img alt="Mock Chat" src={require("../assets/images/mock-chat.PNG")}/>
+
+            </aside>
+            <article>
               <h4>Mock Chat</h4>
               <p>Angularfire chatroom that uses a cookie to collect a username.</p>
-            </aside>
+            </article>
           </div>
         </section>
       </div>
--- src/Components/Subhead.js
+++ src/Components/Subhead.js
@@ -13,40 +13,40 @@ class Subhead extends Component {
     return(
       <div>
         <div>
-          <div className="subhead-head container">
-            <h3>WEB DEVELOPER</h3>
+          <div className="subhead-head">
+            <h3>JUSTIN LATTIMORE</h3>
           </div>
-          <div className="subhead-body container">
-            <h4>Justin Lattimore</h4>
+          <div className="subhead-body slim">
+            <h4>Web Developer</h4>
             <p>Hey, I'm an up and coming Web Developer from North Carolina with a 3D and Graphic Design background. I'm mostly familiar with HTML, CSS, jQuery, React.js and Ruby on Rails.</p>
-            <p>Please feel free to look through my website.</p>
+            <p>Please feel free to look through my projects.</p>
             <ul>
               <li>
-                <a href="https://www.linkedin.com/in/justin-lattimore-90387445/">
+                <a target="_blank" href="https://www.linkedin.com/in/justin-lattimore-90387445/">
                   <i className="ion-social-linkedin"></i>
                   <h5>Linkedin</h5>
                 </a>
               </li>
               <li>
-                <a href="https://github.com/Wyzardsleeves/">
+                <a target="_blank" href="https://github.com/Wyzardsleeves/">
                   <i className="ion-social-github"></i>
                   <h5>Github</h5>
                 </a>
               </li>
               <li>
-                <a href="https://www.instagram.com/justinlattimore/">
+                <a target="_blank" href="https://www.instagram.com/justinlattimore/">
                   <i className="ion-social-instagram-outline"></i>
                   <h5>Instagram</h5>
                 </a>
               </li>
               <li>
-                <a href="https://codepen.io/Wyzardsleeves/">
+                <a target="_blank" href="https://codepen.io/Wyzardsleeves/">
                   <i className="ion-social-codepen"></i>
                   <h5>CodePen</h5>
                 </a>
               </li>
               <li>
-                <a href="https://steemit.com/@justinthedev">
+                <a target="_blank" href="https://steemit.com/@justinthedev">
                   <i className="ion-android-people"></i>
                   <h5>Steemit Blog</h5>
                 </a>
